8a6a22ad8a119fcacbcd99a93f794feeed31fd0d
Jonathan Skantz <jonathanskantz@gmail.com>
2024-02-12 18:17:19 +0100

    Use Go instead of Kotlin

---
+++ internal/graph.go
@@ -0,0 +1,100 @@
+package tsp
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Graph struct {
+	nodes [][]int // adjacency matrix implementation
+}
+
+func NewGraph(numNodes int) Graph {
+
+	nodes := make([][]int, numNodes)
+
+	for i := range nodes {
+		nodes[i] = make([]int, numNodes)
+	}
+
+	return Graph{nodes}
+}
+
+func (g *Graph) addEdge(from int, to int, weight int) {
+	g.nodes[from][to] = weight
+	g.nodes[to][from] = weight
+}
+
+func (g *Graph) removeEdge(from int, to int) {
+	g.nodes[from][to] = 0
+	g.nodes[to][from] = 0
+}
+
+func (g *Graph) hasEdge(from int, to int) bool {
+	return g.nodes[from][to] != 0
+}
+
+func (g *Graph) getWeight(from int, to int) int {
+	return g.nodes[from][to]
+}
+
+// NOTE: `from` may be `to` as well, since the graph is undirected
+func (g *Graph) getNeighbors(from int) []int {
+
+	neighbors := []int{}
+
+	for i := 0; i < len(g.nodes); i++ {
+		if g.hasEdge(from, i) {
+			neighbors = append(neighbors, i)
+		}
+	}
+
+	return neighbors
+
+}
+
+func (g *Graph) getNonEmptyNodes() []int {
+
+	nonEmpty := map[int]struct{}{} // represents a set
+
+	for from := 0; from < len(g.nodes); from++ {
+		for to := 0; to < len(g.nodes); to++ {
+			if g.hasEdge(from, to) {
+
+				_, exists := nonEmpty[to]
+
+				if !exists {
+					nonEmpty[to] = struct{}{}
+				}
+			}
+		}
+	}
+
+	keys := make([]int, 0, len(nonEmpty))
+	for k := range nonEmpty {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
+
+func (g Graph) String() string {
+
+	str := ""
+
+	for from := 0; from < len(g.nodes); from++ {
+		str += fmt.Sprintf("%d: ", from)
+
+		for to := 0; to < len(g.nodes); to++ {
+			if g.hasEdge(from, to) {
+				str += fmt.Sprintf("(%d, %d), ", to, g.getWeight(from, to))
+			}
+		}
+
+		str = strings.TrimSuffix(str, ", ") + "\n"
+	}
+
+	str = strings.TrimSuffix(str, "\n")
+
+	return str
+}
---
+++ internal/solve.go
@@ -0,0 +1,40 @@
+package tsp
+
+import "math/rand"
+
+type Solution struct {
+	cost  int
+	cycle []int
+}
+
+// NOTE: assumes complete graph
+func solveRandom(graph *Graph, startNode int) Solution {
+
+	nodesLeft := graph.getNonEmptyNodes()
+
+	// randomize array
+	for i := range nodesLeft {
+		j := rand.Intn(i + 1)
+		nodesLeft[i], nodesLeft[j] = nodesLeft[j], nodesLeft[i]
+	}
+
+	// place startNode at index 0
+	for i, node := range nodesLeft {
+		if node == startNode {
+			nodesLeft[i], nodesLeft[0] = nodesLeft[0], nodesLeft[i]
+			break
+		}
+	}
+
+	nodesLeft = append(nodesLeft, startNode)
+
+	sol := Solution{cycle: nodesLeft}
+
+	// add cost
+	for i := 1; i < len(nodesLeft); i++ {
+		from, to := nodesLeft[i-1], nodesLeft[i]
+		sol.cost += graph.getWeight(from, to)
+	}
+
+	return sol
+}
