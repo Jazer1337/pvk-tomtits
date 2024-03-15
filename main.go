e6f90f1b88b5923efe3d19ac82e538a1896180ff
Jonathan Skantz <jonathanskantz@gmail.com>
2024-03-16 00:05:40 +0100

    Provide example in main.go

--- main.go
+++ main.go
@@ -7,34 +7,44 @@ import (
 
 func main() {
 
-	// example
+	// // example
 
-	g := tsp.NewGraph()
+	// g := tsp.NewGraph()
 
-	m := []int{
-		0, 1, 50,
-		0, 2, 600,
-		0, 3, 3,
-		0, 4, 500,
-		1, 2, 5,
-		1, 3, 150,
-		1, 4, 7,
-		2, 3, 8,
-		2, 4, 9,
-		3, 4, 10,
-		2, 5, 100,
-		2, 6, 300,
-		5, 6, 200,
-	}
+	// m := []int{
+	// 	0, 1, 50,
+	// 	0, 2, 600,
+	// 	0, 3, 3,
+	// 	0, 4, 500,
+	// 	1, 2, 5,
+	// 	1, 3, 150,
+	// 	1, 4, 7,
+	// 	2, 3, 8,
+	// 	2, 4, 9,
+	// 	3, 4, 10,
+	// 	2, 5, 100,
+	// 	2, 6, 300,
+	// 	5, 6, 200,
+	// }
 
-	// add forward and backward edges
-	for i := 0; i < len(m); i += 3 {
-		g.AddEdge(m[i], m[i+1], m[i+2])
-		g.AddEdge(m[i+1], m[i], m[i+2])
-	}
+	// // add forward and backward edges
+	// for i := 0; i < len(m); i += 3 {
+	// 	g.AddEdge(m[i], m[i+1], m[i+2])
+	// 	g.AddEdge(m[i+1], m[i], m[i+2])
+	// }
 
-	path := g.FindShortestPathAll([]int{0, 4, 3, 1})
+	// path := g.FindShortestPathAll([]int{0, 4, 3, 1})
 
-	fmt.Printf("path: %v\n", path)
+	// fmt.Printf("path: %v\n", path)
+
+	// --- Load JSON ---
+	g1 := tsp.JSONToGraph("internal/mapExample.json")
+	fmt.Println("graph from json:")
+	fmt.Println(g1)
+
+	// --- Generate complete graph ---
+	g2 := tsp.GenerateCompleteGraph(5, 50, 3)
+	fmt.Println("\ncomplete graph:")
+	fmt.Println(g2)
 
 }
