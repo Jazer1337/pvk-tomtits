39c32ceb7aebbdfc31ac517de51161223a6d69df
Johanhah <Johanhah@kth.se>
2024-05-07 14:59:42 +0200

    Update message content and css

--- src/js/metro.js
+++ src/js/metro.js
@@ -46,9 +46,9 @@ export function setup() {
 
             if (currentNode == startNode && garbageCollected()){
                 gameOver = true;
-                document.getElementById("count").innerHTML = "Du kom tillbaka på " + count + " steg! Med alla sopor 😱"
+                document.getElementById("count").innerHTML = "Du kom tillbaka på " + count + " meter! Med alla sopor 😱"
             } else {
-                document.getElementById("count").innerHTML = "Du har åkt " + count + " steg"
+                document.getElementById("count").innerHTML = "Du har åkt " + count + " meter"
             }
         }
         
